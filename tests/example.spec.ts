fcf958930ef2f53aab46ebeab799a5e80cbd4384
fenrir-sunwentao <sunwentao@fenrir-tec.com>
2023-12-15 14:08:36 +0800

    2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("http://localhost:8080");
 
   await expect(page).toHaveTitle("Vite + Vue + TS");
 });
