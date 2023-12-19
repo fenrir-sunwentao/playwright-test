d42e8e73f36c9124a65b4a45eed0061a83d30a86
fenrir-sunwentao <sunwentao@fenrir-tec.com>
2023-12-19 16:31:14 +0800

    6

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,33 @@ test("has title", async ({ page }) => {
 
   await expect(page).toHaveTitle("Vite + Vue + TS");
 });
+test("has title1", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page).toHaveTitle("Vite + Vue + TS");
+});
+test("has title2", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page).toHaveTitle("Vite + Vue + TS");
+});
+test("has title3", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page).toHaveTitle("Vite + Vue + TS");
+});
+test("has title4", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page).toHaveTitle("Vite + Vue + TS");
+});
+test("has title5", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page).toHaveTitle("Vite + Vue + TS");
+});
+test("has title6", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page).toHaveTitle("Vite + Vue + TS");
+});
